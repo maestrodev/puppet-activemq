78e580a80331b3262d38fea3113c381f931ee41a
Brett Porter <brett@apache.org>
2012-04-10 03:20:32 +1000

    fix problem with custom home directories

--- spec/classes/activemq_spec.rb
+++ spec/classes/activemq_spec.rb
@@ -3,11 +3,22 @@ require "#{File.join(File.dirname(__FILE__),'..','spec_helper')}"
 describe 'activemq' do
   let(:facts) { {:architecture => 'x86_64'} }
 
-  context "when using default parameters" do
-    it { should contain_service('activemq').with_ensure('running') }
+  it { should contain_service('activemq').with_ensure('running') }
 
+  context "when using default parameters" do
     it 'should generate valid init.d' do
       expected = IO.read(File.expand_path('activemq-init.d', File.dirname(__FILE__)))
+      content = catalogue.resource('file', '/etc/init.d/activemq').send(:parameters)[:content]
+      content.should == expected
+    end
+  end
+
+  context "when using custom home" do
+    let(:params) { {
+      :home => '/usr/local'
+    } }
+    it 'should generate valid init.d' do
+      expected = IO.read(File.expand_path('activemq-init.d-usrlocal', File.dirname(__FILE__)))
       content = catalogue.resource('file', '/etc/init.d/activemq').send(:parameters)[:content]
       content.should == expected
     end
