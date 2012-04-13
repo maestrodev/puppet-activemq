112115e4b7e53447fee8512883831622ca475295
Brett Porter <brett@apache.org>
2012-04-13 17:37:01 +1000

    remove hard coding from HOME/BASE env vars

--- spec/classes/activemq_spec.rb
+++ spec/classes/activemq_spec.rb
@@ -7,9 +7,18 @@ describe 'activemq' do
 
   context "when using default parameters" do
     it 'should generate valid init.d' do
+      should contain_file("/etc/init.d/activemq")
       expected = IO.read(File.expand_path('activemq-init.d', File.dirname(__FILE__)))
       content = catalogue.resource('file', '/etc/init.d/activemq').send(:parameters)[:content]
       content.should == expected
+    end
+
+    it "should generate a valid wrapper.conf" do
+      should contain_file('wrapper.conf')
+      content = catalogue.resource('file', 'wrapper.conf').send(:parameters)[:content]
+      content.should =~ %r[ACTIVEMQ_HOME=/opt/activemq]
+      content.should =~ %r[ACTIVEMQ_BASE=/opt/activemq]
+      content.should =~ %r[wrapper.java.maxmemory=512]
     end
   end
 
@@ -18,9 +27,29 @@ describe 'activemq' do
       :home => '/usr/local'
     } }
     it 'should generate valid init.d' do
+      should contain_file("/etc/init.d/activemq")
       expected = IO.read(File.expand_path('activemq-init.d-usrlocal', File.dirname(__FILE__)))
       content = catalogue.resource('file', '/etc/init.d/activemq').send(:parameters)[:content]
       content.should == expected
+    end
+
+    it "should generate a valid wrapper.conf" do
+      should contain_file('wrapper.conf')
+      content = catalogue.resource('file', 'wrapper.conf').send(:parameters)[:content]
+      content.should =~ %r[ACTIVEMQ_HOME=/usr/local/activemq]
+      content.should =~ %r[ACTIVEMQ_BASE=/usr/local/activemq]
+    end
+  end
+
+  context "when using custom memory setting" do
+    let(:params) { {
+      :max_memory => '256'
+    } }
+
+    it "should generate a valid wrapper.conf" do
+      should contain_file('wrapper.conf')
+      content = catalogue.resource('file', 'wrapper.conf').send(:parameters)[:content]
+      content.should =~ %r[wrapper.java.maxmemory=256]
     end
   end
 
